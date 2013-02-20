d964553b610963578b5bdd569422b9a95cf6333c
Sanele Mahlalela <saneletm@gmail.com>
2013-02-20 14:27:58 -0500

    minor edits on tmux_new_session.sh

--- tmux_new_session.sh
+++ tmux_new_session.sh
@@ -5,16 +5,16 @@ tmux has-session -t $SESSIONNAME &> /dev/null
 
 if [ $? != 0 ] 
 then
-    tmux new-session -s $SESSIONNAME -n server -d
-    tmux new-window -n dev -d
-    tmux split-window -t server -h
-    tmux new-window -n init -d
-    tmux new-window -n bash -d
-    tmux new-window -n bash -d
-    tmux new-window -n view1 -d
-    tmux new-window -n view2 -d
-    tmux new-window -n templ1 -d
-    tmux new-window -n templ2 -d
+    tmux new-session -s $SESSIONNAME -n server -d  'cd ~/enthenergy/etireports/etirweb; bash -i'
+    tmux new-window -n dev -d   'cd ~/enthenergy/etireports/etirweb; bash -i'
+    tmux split-window -t server -h  
+    tmux new-window -n init -d 'cd ~/enthenergy/etireports/etirweb; bash -i'
+    tmux new-window -n bash -d 'cd ~/enthenergy/etireports/etirweb/etirweb/static; bash -i'
+    tmux new-window -n bash -d 'cd ~/enthenergy/etireports/etirweb/etirweb/static; bash -i'
+    tmux new-window -n view1 -d 'cd ~/enthenergy/etireports/etirweb/etirweb/views; bash -i'
+    tmux new-window -n view2 -d 'cd ~/enthenergy/etireports/etirweb/etirweb/views; bash -i'
+    tmux new-window -n templ1 -d 'cd ~/enthenergy/etireports/etirweb/etirweb/templates; bash -i'
+    tmux new-window -n templ2 -d 'cd ~/enthenergy/etireports/etirweb/etirweb/templates; bash -i'
     tmux new-window -n ipython -d
     #tmux send-keys -t $SESSIONNAME "~/enthenergy/etireports/etirweb" C-m 
   fi
