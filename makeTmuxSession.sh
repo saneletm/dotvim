373f7ac9fe9e3cb45bb2c4409b57dce071a81de0
Sanele Mahlalela <saneletm@gmail.com>
2013-03-13 11:25:18 -0400

    minor edits

--- makeTmuxSession.sh
+++ makeTmuxSession.sh
@@ -6,16 +6,16 @@ tmux has-session -t $SESSIONNAME &> /dev/null
 if [ $? != 0 ] 
 then
     tmux new-session -s $SESSIONNAME -n server -d  'cd ~/enthenergy/etireports/etirweb; bash -i'
-    tmux new-window -n dev -d   'cd ~/enthenergy/etireports/etirweb; bash -i'
+    tmux new-window -n dev -d   'cd ~/enthenergy/etireports/etirweb; workon pyramid; bash -i'
     tmux split-window -t server -h  
     tmux new-window -n init -d 'cd ~/enthenergy/etireports/etirweb; bash -i'
-    tmux new-window -n bash -d 'cd ~/enthenergy/etireports/etirweb/etirweb/static; bash -i'
     tmux new-window -n bash -d 'cd ~/enthenergy/etireports/etirweb/etirweb/static; bash -i'
     tmux new-window -n view1 -d 'cd ~/enthenergy/etireports/etirweb/etirweb/views; bash -i'
     tmux new-window -n view2 -d 'cd ~/enthenergy/etireports/etirweb/etirweb/views; bash -i'
     tmux new-window -n templ1 -d 'cd ~/enthenergy/etireports/etirweb/etirweb/templates; bash -i'
     tmux new-window -n templ2 -d 'cd ~/enthenergy/etireports/etirweb/etirweb/templates; bash -i'
-    tmux new-window -n ipython -d
+    tmux new-window -n dbCons -d 'cd ~/enthenergy/etireports/etirweb; bash -i'
+    tmux new-window -n ipython -d 'cd ~/enthenergy/etireports/etirweb; workon pyramid; ipython; bash -i'
     #tmux send-keys -t $SESSIONNAME "~/enthenergy/etireports/etirweb" C-m 
   fi
 
