cf9c9e81b9ecb3a9b183ca19ad7d9a512bb3d504
Sanele T. Mahlalela <saneletm@gmail.com>
2013-12-31 16:01:06 -0500

    added settings for latex editing (rubber, auctex, reftex)

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -47,6 +47,7 @@
      pylint
      ein
      git-gutter
+     auctex ;; latex menu for file inputs
      )))
 
 (condition-case nill
@@ -223,3 +224,40 @@ See URL `https://github.com/FND/jslint-reporter'."
 
 ;; emacs git gutter to show git diff (use fringe)
 (global-git-gutter-mode +1)
+
+;;LATEX
+;;reftex
+(require 'reftex)  ;; useful for formal paper writing
+
+;;Add support for auctex
+(add-hook 'reftex-load-hook 'imenu-add-menubar-index)
+(add-hook 'reftex-mode-hook 'imenu-add-menubar-index)
+(global-set-key [down-mouse-3] 'imenu)
+
+;;Latex Rubber
+(eval-after-load "tex"
+  '(progn
+     (add-to-list 'TeX-expand-list
+                  '("%(RubberPDF)"
+                    (lambda ()
+                      (if
+                          (not TeX-PDF-mode)
+                          ""
+                        "--pdf"))))
+     (add-to-list 'TeX-command-list
+                '("Rubber" "rubber %(RubberPDF) %t" TeX-run-shell nil t) t)))
+
+;; Rubber short cuts F9, and F12
+(add-hook 'TeX-mode-hook
+          '(lambda ()
+            (define-key TeX-mode-map (kbd "<f9>") ;; compile pdf
+              (lambda ()
+                (interactive)
+                (save-buffer)
+                (TeX-command-menu "Rubber")
+                (TeX-clean)))
+            (define-key TeX-mode-map (kbd "<f7>") ;; view pdf
+              (lambda ()
+                (interactive)
+                (TeX-view)
+                [return]))))
